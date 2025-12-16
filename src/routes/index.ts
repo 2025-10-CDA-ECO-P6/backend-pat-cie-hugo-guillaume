cdc7d2508cef112d50b7086884bbdbae6ae9f58b
Hugo <vault7@v7.is>
2025-12-16 16:47:10 +0100

    fix: ajout des routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,12 @@
 import { Router } from 'express';
 import animalRoutes from './animal.routes';
+import veterinaireRoutes from './veterinaire.routes'; 
+import visiteRoutes from './visite.routes';           
 
 const router = Router();
 
-router.get('/health', (req, res) => {
-  res.status(200).json({ status: 'OK' });
-});
-
-// Montage des routes
 router.use('/animaux', animalRoutes);
-
+router.use('/veterinaires', veterinaireRoutes); 
+router.use('/visites', visiteRoutes);           
 
 export default router;
