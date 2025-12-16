163f78211596eadfe20e3dfccbfbc331dfbc957a
guibrok <gbroquet@gmail.com>
2025-12-16 15:26:10 +0100

    feat: add CRUD endpoints for owners

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import animalRoutes from './animal.routes';
+import ownerRoutes from './owner.routes';
 
 const router = Router();
 
@@ -9,6 +10,7 @@ router.get('/health', (req, res) => {
 
 // Montage des routes
 router.use('/animaux', animalRoutes);
+router.use('/proprietaires', ownerRoutes);
 
 
 export default router;
