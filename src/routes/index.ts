5fc577e54a3ea0d05e9b6bcb3fd6d68f12b6883d
guibrok <gbroquet@gmail.com>
2025-12-17 09:39:54 +0100

    merge: integrate changes from dev

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,20 @@
 import { Router } from 'express';
 import animalRoutes from './animal.routes';
 import ownerRoutes from './owner.routes';
+import veterinaireRoutes from './veterinaire.routes'; 
+import visiteRoutes from './visite.routes';           
+import vaccineRoutes from './vaccine.routes';
+import treatmentRoutes from './treatment.routes';
+
 
 const router = Router();
 
-router.get('/health', (req, res) => {
-  res.status(200).json({ status: 'OK' });
-});
-
-// Montage des routes
 router.use('/animaux', animalRoutes);
 router.use('/proprietaires', ownerRoutes);
+router.use('/veterinaires', veterinaireRoutes); 
+router.use('/visites', visiteRoutes);           
+router.use('/vaccins', vaccineRoutes);
+router.use('/traitements', treatmentRoutes);
 
 
 export default router;
